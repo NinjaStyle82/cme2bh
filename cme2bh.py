1be323909d7e42c27c85ea0accf198cb74f3a960
NinjaStyle82 <10945042+NinjaStyle82@users.noreply.github.com>
2019-10-01 15:52:19 -0600

    fix help and comments
    some comment and help fixes.

--- cme2bh.py
+++ cme2bh.py
@@ -7,14 +7,14 @@ parser = argparse.ArgumentParser(description='Mark Bloodhound owned from CME dat
 parser.add_argument('-c','--creds',  default="neo4j:neo4j", help='Credentials for Neo4j (Default: neo4j:neo4j)')
 parser.add_argument('-s','--server',  default="localhost:7474", help='Server for Neo4j (Default: localhost:7474)')
 parser.add_argument('-f','--fqdn',  help='FQDN for the domain ("ad.domain.com")', required=True)
-parser.add_argument('-n','--netbios', help='Credentials for Neo4j: user:pass', required=True)
-parser.add_argument('-p','--path',  default="./cme.db", help='Path to CME DB file (Default: "./cme.db")')
+parser.add_argument('-n','--netbios', help='NetBIOS domain name ("ad")', required=True)
+parser.add_argument('-p','--path',  default="~/.cme/cme.db", help='Path to CME DB file (Default: "~/.cme/cme.db")')
 
 args = parser.parse_args()
 
 auth = base64.b64encode(args.creds.encode())
 
-# if there is a link in hosts > links then we are admin
+#Run Cypher query in Neo4j
 def runcypher(server,statement,auth):
     headers = { "Accept": "application/json; charset=UTF-8",
                 "Content-Type": "application/json",
@@ -26,11 +26,12 @@ def runcypher(server,statement,auth):
 
 conn = sqlite3.connect(args.path)
 c = conn.cursor()
-# for host with a link in links database (host nas an account with admin), mark as owned
+# for host with a link in links database (host has an account with admin), mark as owned.
 for host in c.execute('SELECT DISTINCT hostname FROM hosts WHERE id IN (SELECT hostid FROM links);'):
     statement = "MATCH (n) WHERE n.name =~ '(?i)^{}\..*$' SET n.owned=true RETURN n".format(host[0])
     runcypher(args.server,statement,auth)
     print('marked computer: {} owned'.format(host[0]))
+# for username in credentials database, mark as owned.
 for user in c.execute("SELECT DISTINCT username FROM credentials WHERE domain LIKE '{}';".format(args.netbios)):
     statement = "MATCH (n) WHERE n.name =~ '(?i)^{}@{}$' SET n.owned=true RETURN n".format(user[0],args.fqdn)
     runcypher(args.server,statement,auth)
